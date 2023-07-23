cb45d3a52560b322088c8555cd29c30d205c01d8
Tomas Bjerre <tomas.bjerre85@gmail.com>
2023-07-23 08:09:13 +0200

    chore: stepping dependencies

--- src/test/java/test/examples/MatchesInlineSnapshotTest.java
+++ src/test/java/test/examples/MatchesInlineSnapshotTest.java
@@ -1,7 +1,6 @@
 package test.examples;
 
 import static org.assertj.snapshot.api.Assertions.assertThat;
-
 import org.assertj.snapshot.api.UpdateMode;
 import org.junit.jupiter.api.Test;
 import test.utils.DummyObject;
@@ -9,65 +8,54 @@ import test.utils.TestDataFactory;
 
 public class MatchesInlineSnapshotTest {
 
-  @Test
-  public void matchesInlineSnapshot1() {
-    final DummyObject whatever = TestDataFactory.createDummyObject();
-    assertThat(whatever)
-        .matchesInlineSnapshot(
-            """
+    @Test
+    public void matchesInlineSnapshot1() {
+        final DummyObject whatever = TestDataFactory.createDummyObject();
+        assertThat(whatever).matchesInlineSnapshot("""
             {
               "someAttr1" : "abc",
               "someAttr2" : 123
             }
             """);
-  }
+    }
 
-  @Test
-  public void matchesInlineSnapshot2() {
-    final DummyObject whatever2 = TestDataFactory.createAnotherDummyObject();
-    assertThat(whatever2)
-        .matchesInlineSnapshot(
-            """
+    @Test
+    public void matchesInlineSnapshot2() {
+        final DummyObject whatever2 = TestDataFactory.createAnotherDummyObject();
+        assertThat(whatever2).matchesInlineSnapshot("""
             {
               "someAttr1" : "def",
               "someAttr2" : 456
             }
             """);
-  }
+    }
 
-  @Test
-  public void matchesInlineSnapshot_double_assert() {
-    final DummyObject whatever1 = TestDataFactory.createDummyObject();
-    assertThat(whatever1)
-        .matchesInlineSnapshot(
-            """
+    @Test
+    public void matchesInlineSnapshot_double_assert() {
+        final DummyObject whatever1 = TestDataFactory.createDummyObject();
+        assertThat(whatever1).matchesInlineSnapshot("""
             {
               "someAttr1" : "abc",
               "someAttr2" : 123
             }
             """);
-    final DummyObject whatever2 = TestDataFactory.createAnotherDummyObject();
-    assertThat(whatever2)
-        .matchesInlineSnapshot(
-            """
+        final DummyObject whatever2 = TestDataFactory.createAnotherDummyObject();
+        assertThat(whatever2).matchesInlineSnapshot("""
             {
               "someAttr1" : "def",
               "someAttr2" : 456
             }
             """);
-  }
+    }
 
-  @Test
-  public void matchesInlineSnapshot_always_update() {
-    final DummyObject whatever1 = TestDataFactory.createDummyObject();
-    assertThat(whatever1)
-        .withUpdateMode(UpdateMode.UPDATE_ALWAYS)
-        .matchesInlineSnapshot(
-            """
+    @Test
+    public void matchesInlineSnapshot_always_update() {
+        final DummyObject whatever1 = TestDataFactory.createDummyObject();
+        assertThat(whatever1).withUpdateMode(UpdateMode.UPDATE_ALWAYS).matchesInlineSnapshot("""
             {
               "someAttr1" : "abc",
               "someAttr2" : 123
             }
             """);
-  }
+    }
 }
